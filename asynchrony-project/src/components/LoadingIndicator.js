d3d264f6b71ccd077922e260b96a82d7ff3054bd
MorenaDlamini <dlaminimorena@gmail.com>
2025-03-23 19:17:02 +0200

    Provides feedback during data loading

---
+++ asynchrony-project/src/components/LoadingIndicator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// loading indicator component
+function LoadingIndicator() {
+  return (
+    <div className="loading">
+      <p>Loading blog posts...</p>
+    </div>
+  );
+}
+
+export default LoadingIndicator;
